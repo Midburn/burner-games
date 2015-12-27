a27978ac192e6e85a2a9a41e429cd658c4b0387d
eladg <ggrelad@gmail.com>
2015-12-27 13:06:16 -0800

    patching production. just bcoz

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -20,7 +20,7 @@ module Api
 
       def cors_preflight_check
         if request.method == "OPTIONS"
-          headers['Access-Control-Allow-Origin'] = 'http://localhost'
+          headers['Access-Control-Allow-Origin'] = 'https://burner-games.herokuapp.com'
           headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
           headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
           headers['Access-Control-Max-Age'] = '1728000'
