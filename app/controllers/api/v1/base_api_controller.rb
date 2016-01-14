dd0887de6fd178718f0ebf9a4683c8082a143b8c
eladg <ggrelad@gmail.com>
2016-01-13 22:52:52 -0800

    allow Access-Control-Allow-Origin *

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -7,25 +7,12 @@ module Api
       ActionController::Parameters.action_on_unpermitted_parameters = :raise
 
       protect_from_forgery
-      before_filter :cors_preflight_check
-      after_filter :cors_set_access_control_headers
 
-      # For all responses in this controller, return the CORS access control headers.
-      def cors_set_access_control_headers
-          headers['Access-Control-Allow-Origin'] = '*'
-          headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-          headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
-          headers['Access-Control-Max-Age'] = "1728000"
-      end
+      after_action :access_control_headers
 
-      def cors_preflight_check
-        if request.method == "OPTIONS"
-          headers['Access-Control-Allow-Origin'] = 'https://burner-games.herokuapp.com'
-          headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-          headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
-          headers['Access-Control-Max-Age'] = '1728000'
-          render :text => '', :content_type => 'text/plain'
-        end
+      def set_access_control_headers
+        headers['Access-Control-Allow-Origin'] = "*"
+        headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
       end
 
       def validate_json
